ed604d78c29c585887bd7d4a6a93c94597a9deb1
andrew <andor305@gmail.com>
2025-11-12 21:47:17 +0200

    project base initiated

---
+++ src/main/java/ua/askerov/routepal/RoutepalApplication.java
@@ -0,0 +1,13 @@
+package ua.askerov.routepal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutepalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoutepalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/askerov/routepal/RoutepalApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.askerov.routepal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutepalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
